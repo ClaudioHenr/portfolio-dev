17c88c5f3f62fa192483a60229eb995a306067b2
stromae <claudio.oliveira10.0p@gmail.com>
2024-04-09 18:16:09 -0300

    build project

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-LF3TX3C2.mjs";import"./chunk-NVEZDFZU.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-AP5GCCKF.mjs";import"./chunk-NVEZDFZU.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
